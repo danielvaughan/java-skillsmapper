b97915f1b5d3ad207e7af6d6ec2c6f0cd5c09ffd
Daniel Vaughan <dan@danielvaughan.com>
2016-11-11 13:52:13 +0000

    deployed to cloud foundry

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 [![Build Status](https://travis-ci.org/danielvaughan/skillsmapper.svg?branch=master)](https://travis-ci.org/danielvaughan/skillsmapper)
 
-# skillsmapper
+# Skills Mapper
+
+## Deployment
+```gradle clean assemble cfPush```
+
+## Deploy URL
+
+http://skills-mapper.cfapps.io/
---
+++ src/main/java/org/codetaming/skillsmapper/PeopleController.java
@@ -0,0 +1,14 @@
+package org.codetaming.skillsmapper;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PeopleController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
