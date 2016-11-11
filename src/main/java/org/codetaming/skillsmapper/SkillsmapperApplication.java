84cd29933eb9ccf0ff06ec4f646f012db4f460bd
Daniel Vaughan <dan@danielvaughan.com>
2016-11-11 13:07:02 +0000

    initial commit

---
+++ src/main/java/org/codetaming/skillsmapper/SkillsmapperApplication.java
@@ -0,0 +1,12 @@
+package org.codetaming.skillsmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsmapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsmapperApplication.class, args);
+	}
+}
---
+++ src/test/java/org/codetaming/skillsmapper/SkillsmapperApplicationTests.java
@@ -0,0 +1,16 @@
+package org.codetaming.skillsmapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkillsmapperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
