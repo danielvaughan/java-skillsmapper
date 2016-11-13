2b8f9bb08bfd656668c2d871ece678a962393b11
Daniel Vaughan <dan@danielvaughan.com>
2016-11-13 11:18:05 +0000

    loaded neo4j URL from environment variables to enable travis ci to work without checking in credentials

--- src/main/java/org/codetaming/skillsmapper/MyNeo4jConfiguration.java
+++ src/main/java/org/codetaming/skillsmapper/MyNeo4jConfiguration.java
@@ -2,6 +2,7 @@ package org.codetaming.skillsmapper;
 
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
@@ -17,8 +18,21 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableNeo4jRepositories(basePackages = "org.codetaming.skillsmapper.repositories")
 public class MyNeo4jConfiguration extends Neo4jConfiguration {
 
+    public static final String URL = System.getenv("NEO4J_URL") != null ? System.getenv("NEO4J_URL") : "http://neo4j:neo4j@localhost:7474";
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+        config
+                .driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
+                .setURI(URL);
+        System.out.println(config.toString());
+        return config;
+    }
+
     @Override
     public SessionFactory getSessionFactory() {
-        return new SessionFactory("org.codetaming.skillsmapper.domain");
+        return new SessionFactory(getConfiguration(), "org.codetaming.skillsmapper.domain");
     }
 }
